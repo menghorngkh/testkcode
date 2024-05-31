ab6eb2974ff8e238b08fad294f9c5b4b2571bbb2
menghorngkh <menghorng4636@gmail.com>
2024-05-31 16:58:41 +0700

    change

--- js/app.js
+++ js/app.js
@@ -5,38 +5,4 @@ function OpenNav(){
         document.getElementById("nav").style.width = "0%";
 }
 
-// $(document).ready(function() {
-//     $('.expand-collapse h3').each(function() {
-//         var tis = $(this), state = false, answer = tis.next('div').slideUp();
-//         tis.click(function() {
-//             state = !state;
-//             answer.slideToggle(state);
-//             tis.toggleClass('active',state);
-//         });
-//     });
-// });
-
-// $(function(){
-//     $("#big-image img:eq(0)").nextAll().hide();
-//     $(".small-images div").click(function(e){
-//         var index = $(this).index();
-//         $("#big-image img").eq(index).show().siblings().hide();
-//     });
-// });
-
-// function addClassElementEvent(element,className,event){
-  
-//     let elements = document.querySelectorAll(element);
-  
-//     for(var i = 0; i<elements.length; i++) {
-//         elements[i].addEventListener(event, function(event) {
-//             [].forEach.call(elements, function(el) {
-//                   el.classList.remove(className);
-//              });
-//              this.classList.add(className);
-//         });
-//     }
-//   }
-  
-//   addClassElementEvent('.change','active','click');
 
